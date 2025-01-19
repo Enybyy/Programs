12a86b652f9a3ace73b66d5e3e65432aade5f38f
EliAnn <rmeliud@gmail.com>
2025-01-19 11:39:42 -0500

    delete js

--- Automatizacion/Web/Automatizar_RH/static/js/script.js
+++
@@ -1,4 +0,0 @@
-// Mostrar el loader al enviar el formulario
-document.getElementById("process-form").addEventListener("submit", function () {
-    document.getElementById("loader").style.display = "flex";
-  });
