5dca0d5624edb917a86251f64d4dd91a383b52d7
EliAnn <rmeliud@gmail.com>
2025-01-19 10:27:03 -0500

    udpate index

---
+++ Automatizacion/Web/Automatizar_RH/static/js/script.js
@@ -0,0 +1,4 @@
+// Mostrar el loader al enviar el formulario
+document.getElementById("process-form").addEventListener("submit", function () {
+    document.getElementById("loader").style.display = "flex";
+  });
